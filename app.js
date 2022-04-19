7052e38bfce7d4a472d820659b08372bb2e9425a
basv1996 <bas.vugts@gmail.com>
2022-04-19 11:23:18 +0200

    first commit / working chat room

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const path = require('path')
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4242
+
+
+server.listen(4242, () => {
+  console.log('listening on *:4242');
+});
+
+//this will sorve static files from public folder
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// // //using static files
+app.use(express.static(__dirname + "/public"));
+
+
+function logger(req, res, next){
+    console.log(req.originalUrl)
+    next()
+}
+
+app.get('/', (req, res) => {
+  res.render('pages/index');
+});
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
