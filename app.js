59f91cefccc7bb512dc5697405fde66348dca71c
basv1996 <bas.vugts@gmail.com>
2022-04-19 16:36:07 +0200

    username added to new user

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const path = require('path')
 require("dotenv").config();
 
 let userCount=0
+const users = {}
 
 const PORT = process.env.PORT || 4242
 
@@ -40,21 +41,28 @@ app.get('/', (req, res) => {
 
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  //console.log('a user connected');
+  //console.log(socket.id);
   userCount++
   io.emit('usercnt', userCount)
 
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+    //socket.broadcast
+  })
+
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    //console.log('user disconnected');
     userCount--
     io.emit('usercnt', userCount)
+    delete users[socket.id]
   });
 
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+
+  socket.on('chat-message', (msg) => {
+    socket.broadcast.emit('chat-message', { msg: msg, name: users[socket.id] })
+    //io.emit('chat message', msg);
   });
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
 });
